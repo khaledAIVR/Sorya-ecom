f4d5b4fab24c02c7437e1e555c40a7c0652d12bb
khaledAIVR <48943763+khaledAIVR@users.noreply.github.com>
2021-12-14 04:01:08 +0200

    Added categories UI to the main page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Navbar } from "../components/Navbar";
 import { Slider } from "../components/Slider";
 import { Announcement } from "../components/Announcement";
+import Categories from "../components/Categories";
 export const Home = () => {
   return (
     <>
       <Announcement />
       <Navbar />
       <Slider />
+      <Categories />
     </>
   );
 };
